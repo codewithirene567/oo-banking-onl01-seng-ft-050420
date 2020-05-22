172192bcfca29145ae58c27fddcf01712f3bdc99
Irene Scott <irenejinscott@gmail.com>
2020-05-22 02:37:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,8 @@ end
 def reverse_transfer
   if @status == "complete"
     @sender.balance += @amount
-    @receiver.balance
+    @receiver.balance -= @amount
+    @status = "reversed"
     
 end
 
