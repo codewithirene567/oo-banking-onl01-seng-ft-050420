8f276e431d9b19bc3bde0a0cf87db0a641caa0b5
Irene Scott <irenejinscott@gmail.com>
2020-05-22 02:25:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ else
 def execute_transaction
   if @sender.balance > @amount && @status == "pending" && valid? 
     @sender.balance -= add_amount
-    @receiver.balence += @add_amount
+    @receiver.balence += @amount
     "complete"
   #sender is giving to the receiver
 else
